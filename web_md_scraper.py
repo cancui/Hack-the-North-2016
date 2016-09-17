18d2c52df8a283720f73ccca56a4dc68327b82b2
Can Cui <can.cui888@gmail.com>
2016-09-17 11:09:14 -0400

    inital webscraper

---
+++ web_md_scraper.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib2
+#create a beautiful soup object to extract the URL information
+#apply beautiful soup functions on URL
+
+url = 
+
+
+if __name__ == '__main__':
+	main()
