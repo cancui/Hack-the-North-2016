46128e5a7c7f294c5c5be43ef75ae0c875c5af5f
MikaelaMacMahon <mikaela.macmahon@gmail.com>
2016-09-18 01:23:43 -0400

    finished symptom retreival and began treatment acquisition

--- web_md_scraper.py
+++ web_md_scraper.py
@@ -6,16 +6,60 @@ from pprint import pprint
 #create a beautiful soup object to extract the URL information
 #apply beautiful soup functions on URL
 
-def get_wat_in()
-	watson_input = "heart" #get value from watson
-	return watson_input
+def get_stroke_symp():
+	symptoms = ["speech distortion", "lopsided smile", "only able to lift one arm", "loss of feeling", "difficulties speaking", "numbness", "stroke"]
+	symp_list = symptoms.split(" ")
+	return symp_list
+
+def get_choking_symp():
+	symptoms = ["difficulty breathing", "coughing", "wheezing", "bent over", "choking"]
+	symp_list = symptoms.split(" ")
+	return symptom_list
+
+def get_bleeding_symp():
+	symptoms = ["extensive bleeding", "pain", "faintness", "shock", 'lightheadedness']
+	symp_list = symptoms.split(" ")
+	return symp_list
+
+def get_spinal_symp():
+	symptoms = ["pain in back or neck", "usual shape or twist in the spine", "soreness", "bruising", "loss of limb control", 
+		"loss of sensation", "burning", "tingling", "loss of bladder or bowel control", "breathing difficulties"]
+	symp_list = symptoms.split(" ")
+	return symp_list
+
+def initializeCPR():
+	#after establishing that they are unresponsive
+	print "Try shaking their shoulder and calling to them. Are they responsive?"
+	if raw_input().lower() == "no":
+		print "Have you called 911?"
+		if raw_input().lower() == "no":
+			print "Please locate the nearest cellular device, call 911 and inform them of the situation."
+			print "Respond with OK when ready"
+		if raw_input().lower() == "ok":
+			print "Lower your head above their's, and listen to their breathing"
+			print "Is their airway open and clear?"
+			if raw_input().lower() == "no":
+				pass
+				#continue with cpr
+			elif raw_input().lower() == "yes":
+				pass
+				#check other symptoms
+
+def get_treatment(identifier, specifier):
+	url = "http://www.sja.org.uk/sja/first-aid-advice/" + identifier + "/" + specifier + ".aspx"
+	content = urllib2.urlopen(urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read()
+	soup = BeautifulSoup(content, "html.parser")
+	#treatment = soup.find("div", "col-sm-12")
+	treatment = soup.findAll("p").
+	treatment2 = str(treatment)
+	treatment3 = treatment2.split("What to do if")
+	print treatment3[1]
 
 def get_symptoms(outer_id, inner_id):
 	url_outid = outer_id.replace(" ", "-") 
 	url_inid = inner_id.replace(" ", "-")
-
 	#specify the URL for the first category of possible injuries
-	url = "http://www.sja.org.uk/sja/first-aid-advice/" + url_outid + "/" + url_inid ".aspx"
+	url = "http://www.sja.org.uk/sja/first-aid-advice/" + url_outid + "/" + url_inid + ".aspx"
 	content = urllib2.urlopen(urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read()
 	#parse html and store in 'BeautifulSoup' format
 	soup = BeautifulSoup(content, "html.parser")
@@ -24,42 +68,34 @@ def get_symptoms(outer_id, inner_id):
 	symps = []
 	for item in symptoms:
 		symps.append(item.renderContents())
-		print item.renderContents()
+		#print item.renderContents()
 
 	symps2 = []
 	for item in symps:
 		temp1 = item.replace("\n", " ")
 		temp2 = temp1.replace("\r", "")
 		temp3 = temp2.replace("\xe2\x80\x93", "")
-		symps2.append(temp3[3:]) #cuts off the numbers 
+		temp4 = temp3.replace("\xc2\xa0", " ")
+		symps2.append(temp4[3:]) #cuts off the numbers 
 
 # for each item take each word and insert into a list
 	symps_words = []
+	temp_str = ""
+	temp_str2 = ""
+	temp_str3 = ""
 	for string in symps2:
-		for item in string:
-			symps_words.append(item)
+		temp_str = string.replace("(", "")
+		temp_str2 = temp_str.replace(")", "")
+		temp_str3 = temp_str2.replace("/", " ")
+		symps_words += temp_str3.split(" ")
 
-	#str(symptoms)
+	symps_words = [item.lower() for item in symps_words]
 	print symps_words
-
 	return symps_words
-
-def get_stoke_symp():
-
-def get_choking_symp():
-
-def get_bleeding_symp():
-
-def get_spinal_symp():
-
-def get_diabetic_symp():
-
-def initializeCPR():
-
 
 def main():
 
-	watson_input = get_wat_in()
+	watson_input = "heart" #recieve from watson
 
 	h_symp1 = []
 	h_symp2 = []
@@ -114,27 +150,24 @@ def main():
 		hc_symp4 = get_symptoms(watson_input, "heatstroke")
 		hc_symp5 = get_symptoms(watson_input, "hypothermia")
 		cur_symp = hc_symp1 + hc_symp2 + hc_symp3 + hc_symp4 + hc_symp5
-	elif watson_input is "poisoning":
-		p_symp1 = get_symptoms(watson_input, "alcohol posioning")
-		cur_symp = p_symp1
 	elif watson_input is "illnesses and conditions":
 		ic_symp1 = get_symptoms(watson_input, "asthma attack")
 		ic_symp2 = get_symptoms(watson_input, "allergic reactions")
-		ic_symp3 = get_stoke_symp()
+		ic_symp3 = get_stroke_symp()
 		ic_symp4 = get_symptoms(watson_input, "shock")
 		ic_symp5 = get_symptoms(watson_input, "seizures fits in adults")
-		ic_symp1 = get_diabetic_symp()
 		cur_symp = ic_symp1 + ic_symp2 + ic_symp3 + ic_symp4 + ic_symp5
 	elif watson_input is "loss of responsiveness":
 		#might be sleeping or having a heart beat 
 		initializeCPR() #could just ask questions
-		return #when main is no longer the function identifier
+		#return #when main is no longer the function identifier
 
-	
-	#req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	#query the website and return the html to the variable 'content'
-	req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	content = urllib2.urlopen(req).read()
+	#retrieve specified medical diagnosis
+	#replace lower line
+	specifier = "heart-attack"
+	get_treatment(watson_input, specifier)
+
+
 	#parse html and store in 'BeautifulSoup' format
 	#soup = BeautifulSoup(content, "html.parser")
 	#look at nested structure of html page
